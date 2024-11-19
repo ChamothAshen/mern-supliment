8d2bc1192282e90039f91edff9c04380d617a72e
chamothAshen <153191511+chamothAshen@users.noreply.github.com>
2024-11-19 23:39:42 +0530

    create singn-up api route

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
+import authRouter  from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -13,8 +15,16 @@ mongoose.connect(process.env.MONGO)
 
 
 const app  = express();
+app.use(express.json());
+
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
   });
+
+  
   
