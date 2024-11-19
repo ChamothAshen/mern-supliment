8f30d0da8ca64198a5d5d714df681747a3382298
chamothAshen <153191511+chamothAshen@users.noreply.github.com>
2024-11-19 20:11:53 +0530

    create header compornent

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import Singnin from './pages/Singnin';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Heder from './component/Heder';
 
 export default function App() {
   return   <BrowserRouter>
+      <Heder/>
        <Routes>
        <Route path='/' element={<Home />} />
        <Route path='/sing-in' element={<Singnin />} />
