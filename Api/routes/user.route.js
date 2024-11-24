9c741b739aa3623556703007309a25f0977ef9cc
chamothAshen <153191511+chamothAshen@users.noreply.github.com>
2024-11-25 00:08:48 +0530

    create user update api

--- Api/routes/user.route.js
+++ Api/routes/user.route.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import { test,updateUser } from '../controller/user.controller.js';
+import { verifyToken } from '../utills/verifyUser.js';
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("hello world");
-});
+router.get('/test', test);
+router.post('/update/:id',verifyToken, updateUser)
+//router.delete('/delete/:id', deleteUser)
+//router.get('/listings/:id', getUserListings)
+//router.get('/:id', getUser)
 
 export default router; 
