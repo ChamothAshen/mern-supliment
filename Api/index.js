89d3602c4609018e002e5e0590cbb42b5af8a6f8
chamothAshen <153191511+chamothAshen@users.noreply.github.com>
2024-11-30 10:10:57 +0530

    add free instalation

--- Api/index.js
+++ Api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter  from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js'
+import path from 'path';
  dotenv.config();
   mongoose.connect(process.env.MONGO)
    .then(() => {
@@ -14,6 +15,9 @@ import listingRouter from './routes/listing.route.js'
     console.log(err);
   });
 
+ const __dirname = path.resolve();
+
+
 
 const app  = express();
 app.use(express.json());
@@ -22,6 +26,12 @@ app.use(cookieParser());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter,)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 app.listen(3000, () => {
